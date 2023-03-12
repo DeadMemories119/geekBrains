a0bede5db0d33c95f51de3ae1367c8096dce5a0c
Aleksandr Morozov <deadmemories119@gmail.com>
2023-03-12 18:33:35 -0400

    Added first seminar's homework

---
+++ homeWork/seminar1/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// Console.WriteLine($"{num1} is the bigger than {num2}");
+
+// if (num2 > num1)
+// Console.WriteLine($"{num2} is the bigger than {num1}");
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input third number");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// var max = num1;
+
+// if (num2 > num1) 
+// {
+// int max = num2;
+// }
+
+// if (num3 > max)
+// {
+// int max = num3;
+// }
+
+// // почему не даёт воспользоваться переменной max в условии?
+// // ни var, ни int ничего не меняют 
+
+// Console.WriteLine($"{max} is the biggest number");
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+
+// Console.WriteLine("Input a number");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 2 = 0)
+//  {
+// Console.WriteLine($"{number} is even number");
+// }
+
+// if (number % 2 = 1)
+//  {
+// Console.WriteLine($"{number} is not even number");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Input a number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+
+// while (count >= number) 
+// {
+//     if (count % 2 = 0) 
+//     {
+//         Console.WriteLine($" {count} ");
+//     }
+//     count++;
+
+// }
